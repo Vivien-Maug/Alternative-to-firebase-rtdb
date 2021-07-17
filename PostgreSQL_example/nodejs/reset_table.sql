d45bd716c0b63124f847c240ea0b4d6e5f0a59a7
Vivien <47042471+Vedoc@users.noreply.github.com>
2021-07-17 23:22:48 +0200

    Add script to reset the database

---
+++ PostgreSQL_example/nodejs/reset_table.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS issue;
+DROP TABLE IF EXISTS member;
+
+CREATE TABLE member (
+    member_id               serial PRIMARY KEY,
+    member_name             VARCHAR ( 30 ) NOT NULL
+);
+
+CREATE TABLE issue (
+    issue_id             serial PRIMARY KEY,
+    issue_name           VARCHAR ( 50 ) NOT NULL,
+    issue_description    VARCHAR ( 255 ),
+    member_id            INT ,
+    FOREIGN KEY (member_id)
+        REFERENCES member (member_id)
+);
+
+INSERT INTO member (member_name) VALUES
+    ('Vivien'), 
+    ('Aria'), 
+    ('Paul'), 
+    ('Anne');
+
+INSERT INTO issue (issue_name, issue_description, member_id) VALUES
+    ('Bad UI Display', 'In contact view, the text "click here" is not aligned.', 1), 
+    ('Bad translation', 'blabla description', 2), 
+    ('Issue Name num 3', 'Issue', 3);
+
+
